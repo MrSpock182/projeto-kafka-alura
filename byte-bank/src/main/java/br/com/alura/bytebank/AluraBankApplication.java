b24b50e53b7ec1068fa354894f8e7bae52f54763
MrSpock182 <klebernunes182@gmail.com>
2023-01-09 23:48:22 -0300

    aula4.2

--- byte-bank/src/main/java/br/com/alura/bytebank/AluraBankApplication.java
+++ byte-bank/src/main/java/br/com/alura/bytebank/AluraBankApplication.java
@@ -1,23 +1,13 @@
 package br.com.alura.bytebank;
 
-import br.com.alura.bytebank.messaging.EnvioPix;
-import br.com.alura.bytebank.messaging.ProdutorPixBancoCentral;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
-import java.math.BigDecimal;
-
 @EnableKafka
 @SpringBootApplication
 public class AluraBankApplication implements CommandLineRunner {
-
-    private final ProdutorPixBancoCentral produtor;
-
-    public AluraBankApplication(ProdutorPixBancoCentral produtor) {
-        this.produtor = produtor;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AluraBankApplication.class, args);
@@ -25,10 +15,5 @@ public class AluraBankApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        produtor.envioMensagem(
-                new EnvioPix(
-                        "produtor",
-                        "quemrecebe",
-                        BigDecimal.valueOf(10.0)));
     }
 }
