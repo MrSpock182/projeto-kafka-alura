fc0fb098ab274c7bb68006f51524a22c2ba4d2aa
MrSpock182 <klebernunes182@gmail.com>
2023-01-10 01:22:05 -0300

    aula2.1

--- byte-bank/src/main/java/br/com/alura/bytebank/ByteBankApplication.java
+++ byte-bank/src/main/java/br/com/alura/bytebank/ByteBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AluraBankApplication {
+public class ByteBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AluraBankApplication.class, args);
+		SpringApplication.run(ByteBankApplication.class, args);
 	}
 
 }
